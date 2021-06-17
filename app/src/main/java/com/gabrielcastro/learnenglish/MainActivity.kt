e7216071315d39161af3f89fc460095bd211a65e
Gabriel Castro <gabrielcssg@gmail.com>
2021-06-16 22:56:33 -0300

    first commit: configs and animals complete

---
+++ app/src/main/java/com/gabrielcastro/learnenglish/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.gabrielcastro.learnenglish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.gabrielcastro.learnenglish.fragment.AnimalsFragment
+import com.gabrielcastro.learnenglish.fragment.NumbersFragment
+import com.gabrielcastro.learnenglish.fragment.VowelsFragment
+import com.ogaclejapan.smarttablayout.SmartTabLayout
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.elevation = 0F;
+
+        val smartTabLayout = findViewById<SmartTabLayout>(R.id.smartTabLayout)
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+
+        val adapter = FragmentPagerItemAdapter(
+            this.supportFragmentManager,
+            FragmentPagerItems.with(this)
+                .add(FragmentPagerItem.of("Animals", AnimalsFragment::class.java))
+                .add(FragmentPagerItem.of("Numbers", NumbersFragment::class.java))
+                .add(FragmentPagerItem.of("Vowels", VowelsFragment::class.java))
+                .create()
+        )
+
+        viewPager.adapter = adapter
+        smartTabLayout.setViewPager(viewPager)
+    }
+}
---
+++ app/src/main/java/com/gabrielcastro/learnenglish/fragment/AnimalsFragment.kt
@@ -0,0 +1,79 @@
+package com.gabrielcastro.learnenglish.fragment
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import com.gabrielcastro.learnenglish.R
+
+
+class AnimalsFragment : Fragment(), View.OnClickListener {
+    lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_animals, container, false)
+
+        val buttonDog = view.findViewById<ImageButton>(R.id.btn_dog)
+        val buttonCat = view.findViewById<ImageButton>(R.id.btn_cat)
+        val buttonLion = view.findViewById<ImageButton>(R.id.btn_lion)
+        val buttonMonkey = view.findViewById<ImageButton>(R.id.btn_monkey)
+        val buttonSheep = view.findViewById<ImageButton>(R.id.btn_sheep)
+        val buttonCow = view.findViewById<ImageButton>(R.id.btn_cow)
+
+        buttonDog.setOnClickListener(this)
+        buttonCat.setOnClickListener(this)
+        buttonLion.setOnClickListener(this)
+        buttonMonkey.setOnClickListener(this)
+        buttonSheep.setOnClickListener(this)
+        buttonCow.setOnClickListener(this)
+
+        return view
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) { // switch
+            R.id.btn_dog -> {
+                mediaPlayer = MediaPlayer.create(activity, R.raw.dog)
+                playSong()
+            }
+            R.id.btn_cat -> {
+                mediaPlayer = MediaPlayer.create(activity, R.raw.cat)
+                playSong()
+            }
+            R.id.btn_lion -> {
+                mediaPlayer = MediaPlayer.create(activity, R.raw.lion)
+                playSong()
+            }
+            R.id.btn_monkey -> {
+                mediaPlayer = MediaPlayer.create(activity, R.raw.monkey)
+                playSong()
+            }
+            R.id.btn_sheep -> {
+                mediaPlayer = MediaPlayer.create(activity, R.raw.sheep)
+                playSong()
+            }
+            R.id.btn_cow -> {
+                mediaPlayer = MediaPlayer.create(activity, R.raw.cow)
+                playSong()
+            }
+        }
+    }
+
+    private fun playSong() {
+        mediaPlayer.start()
+        mediaPlayer.setOnCompletionListener {
+            mediaPlayer.release()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+}
